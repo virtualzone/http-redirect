4236e4ac91b14f6444cf51869619f5c68818cee9
virtualzone <13085858+virtualzone@users.noreply.github.com>
2022-06-17 08:09:55 +0200

    Logging

--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"strings"
 )
@@ -8,13 +9,17 @@ import (
 type Handler struct{}
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	if !strings.HasPrefix(path, "/") {
+		path = "/" + path
+	}
 	target := GetConfig().TargetURL
 	if GetConfig().AppendPath {
-		path := strings.TrimPrefix(r.URL.Path, "/")
-		if path != "" {
-			target += "/" + path
+		if path != "/" {
+			target += path
 		}
 	}
+	log.Printf("%d redirect: %s -> %s\n", GetConfig().HttpStatusCode, path, target)
 	w.Header().Set("Location", target)
 	w.WriteHeader(GetConfig().HttpStatusCode)
 }
